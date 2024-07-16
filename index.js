7aef8cec23afced9c0a7263f910476476f74031f
Tyler <tylermavan@gmail.com>
2024-07-15 22:00:03 -0500

    lots of changes

--- index.js
+++
@@ -1,7 +0,0 @@
-function scrollToElement(e) {
-  console.log(e);
-  console.log(e.getAttribute("section"));
-  const sectionID = e.getAttribute("section");
-  const sectionElement = document.getElementById(sectionID);
-  sectionElement.scrollIntoView();
-}
